ded51f40348c14d347d0be81ff91fa4c6098bf26
Zakher Masri <masrizakher@gmail.com>
2019-02-11 18:54:11 +0300

    wavy pattern

--- src/components/drawing.js
+++ src/components/drawing.js
@@ -1,65 +1,47 @@
 import React from "react";
 import Paper from "paper";
-import { getRandomArbitrary } from "../util";
 
 class Drawing extends React.Component {
   componentDidMount() {
     Paper.setup(this.canvas);
-
-    this.makeGrid(20, 20);
+    // let bg = new Paper.Path.Rectangle(
+    //   new Paper.Point(0, 0),
+    //   new Paper.Point(
+    //     Paper.project.view.bounds.width,
+    //     Paper.project.view.bounds.height
+    //   )
+    // );
+    // bg.fillColor = "red";
+    let divising = 20;
+    this.makeGrid(window.innerWidth / divising, window.innerHeight / divising);
+    // this.makeGrid(10,10);
     let circleGroup = Paper.project.activeLayer.children[0];
 
     circleGroup.children.map((cell, i) => {
-      // let circle = new Paper.Path.Circle(cell.bounds.center, 5);
-      // circle.strokeWidth = 2;
-      // let dot = new Paper.Path.Circle(circle.getPointAt(0), 5);
-      // dot.fillColor = "white";
-      // let offset = 0;
-      // let movement = getRandomArbitrary(0.1, 0.8);
-      // dot.onFrame = function() {
-      //   this.bounds.center.set(circle.getPointAt(offset));
-      //   offset += movement;
-      //   if (offset > circle.length) {
-      //     offset = 0;
-      //   }
-      // };
-      // return circle;
-      let path = new Paper.Path(
-        cell.bounds.topLeft,
-        new Paper.Point(cell.bounds.topRight.x, cell.bounds.topRight.y)
-      );
+      let path = new Paper.Path.Circle(cell.bounds.center, 5);
       // path.strokeColor = "red";
-      // path.strokeWidth = 1;
+      // path.strokeWidth = 5;
       // path.strokeCap = "round";
-      path.bounds.center.set(cell.bounds.center);
+
       let offset = 0;
-      // let n = 45;
-      // path.rotate(i * n);
-      let cir = new Paper.Path.Circle(path.getPointAt(path.length), 2);
+      let n = 35;
+      path.rotate(n * i);
+      let cir = new Paper.Path.Circle(path.getPointAt(path.length - 1), 2);
       cir.fillColor = "white";
-      let forwardMvmnt = true;
       cir.onFrame = function() {
-        if (offset === 0) {
-          forwardMvmnt = true;
-        } else if (offset === Math.round(path.length) - 1) {
-          forwardMvmnt = false;
-          // offset--;
-        }
-        if (forwardMvmnt === true) {
+        if (offset <= Math.round(path.length - 1)) {
           this.bounds.center.set(path.getPointAt(offset));
           offset++;
-        } else if (forwardMvmnt === false) {
-          this.bounds.center.set(path.getPointAt(offset));
-          offset--;
+        } else {
+          offset = 0;
         }
       };
-      path.onFrame = function() {
-        this.rotation += 0.01 * i;
-        // console.log(this.rotation);
-      };
-      // path.rotate(i * 166);
-      return cir;
+
+      return cell;
     });
+    // circleGroup.scale(0.2);
+    // Paper.project.view.scale(0.8);
+    Paper.project.activeLayer.scale(0.8);
   }
 
   makeGrid = (cols, rows) => {
@@ -100,8 +82,8 @@ class Drawing extends React.Component {
           ref={ref => {
             this.canvas = ref;
           }}
-          width={500}
-          height={500}
+          width={window.innerWidth - 100}
+          height={window.innerHeight - 100}
           className="backgroundCanvas"
         />
       </div>
